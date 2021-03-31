cd348a9912372e10b90766356578f8b5051c7c67
Kevin Brill <kevin.brill@gmail.com>
2021-03-31 13:12:22 -0400

    Corrected regex matching

--- moment.js
+++ moment.js
@@ -2160,8 +2160,8 @@
     // 0000-00-00 0000-W00 or 0000-W00-0 + T + 00 or 00:00 or 00:00:00 or 00:00:00.000 + +00:00 or +0000 or +00)
     var extendedIsoRegex = /^\s*((?:[+-]\d{6}|\d{4})-(?:\d\d-\d\d|W\d\d-\d|W\d\d|\d\d\d|\d\d))(?:(T| )(\d\d(?::\d\d(?::\d\d(?:[.,]\d+)?)?)?)([\+\-]\d\d(?::?\d\d)?|\s*Z)?)?$/;
     var basicIsoRegex = /^\s*((?:[+-]\d{6}|\d{4})(?:\d\d\d\d|W\d\d\d|W\d\d|\d\d\d|\d\d))(?:(T| )(\d\d(?:\d\d(?:\d\d(?:[.,]\d+)?)?)?)([\+\-]\d\d(?::?\d\d)?|\s*Z)?)?$/;
-    var completePrecisionIsoRegex = /\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:(00|30|45)\d|Z)/;
-    var noMillisecondsIsoRegex = /\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d([+-][0-2]\d:(00|30|45)\d|Z)/;
+    var completePrecisionIsoRegex = /\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+(([+-][0-2]\d:00|30|45)|Z)/;
+    var noMillisecondsIsoRegex = /\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\+(([+-][0-2]\d:00|30|45)|Z)/;
 
     var tzRegex = /Z|[+-]\d\d(?::?\d\d)?/;
 
